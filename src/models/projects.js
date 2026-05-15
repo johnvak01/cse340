a04e75dcd364f10bd39613255970f5e189790e61
johnvak01 <johnvak01@gmail.com>
2026-05-14 19:17:16 -0600

    fixed sql query

--- src/models/projects.js
+++ src/models/projects.js
@@ -2,7 +2,7 @@ import db from './db.js'
 
 const getAllServiceProjects = async () => {
     const query = `
-        SELECT PROJECT_NAME,
+        SELECT PROJECT_Title,
 		ORGANIZATION_NAME,
       FROM public.service_project join public.organization on organization.organization_id = service_project.organization_id;
     `;
